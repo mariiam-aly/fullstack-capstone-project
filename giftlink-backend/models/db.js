c2e834bd4456fe73e312231f333cb566c9dbc095
mariiam-aly <xmariam.aly@gmail.com>
2024-12-28 07:06:16 -0500

    first commit

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -18,7 +18,7 @@ async function connectToDatabase() {
     // Task 1: Connect to MongoDB
     await client.connect()
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    const dbInstance = client.db(dbName); 
+    dbInstance = client.db(dbName); 
     // Task 3: Return database instance
     return dbInstance;
 }

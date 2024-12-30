9b6ab6a81e7d869f6695af20606cfc2915e3cc1c
mariiam-aly <xmariam.aly@gmail.com>
2024-12-30 07:20:32 -0500

    minor fixes

--- giftlink-frontend/src/components/SearchPage/SearchPage.js
+++ giftlink-frontend/src/components/SearchPage/SearchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {urlConfig} from '../../config';
+import './SearchPage.css'
 
 function SearchPage() {
     const [searchQuery, setSearchQuery] = useState('');
